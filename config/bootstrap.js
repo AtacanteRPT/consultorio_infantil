9799ccc19b8a63a9157d3991d764ee9bb6f1d89a
AtacanteRPT <rpaucara13@umsa.bo>
2019-11-21 18:16:46 +0000

    sin eliminar 2

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -30,6 +30,8 @@ module.exports.bootstrap = async function(done) {
 
   // Don't forget to trigger `done()` when this bootstrap function's logic is finished.
   // (otherwise your server will never lift, since it's waiting on the bootstrap)
+
+sails.config.port=3000
   return done();
 
 };
